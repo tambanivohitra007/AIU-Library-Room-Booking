486014f5392361d551ec61f511a75ba2dd8c0461
Rindra Ambinintsoa Razafinjatovo <rindra.it@gmail.com>
2026-01-21 09:39:27 +0700

    chore: Delete a file.

--- server/prisma/migrations/20260120223423_add_semester_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Semester" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "startDate" DATETIME NOT NULL,
-    "endDate" DATETIME NOT NULL,
-    "isActive" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
